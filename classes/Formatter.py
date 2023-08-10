c1aa7acc6e58340803a4c2b5adb5d083c60018e2
Jade Burgin <jjr1945@rit.edu>
2023-08-10 14:58:36 -0400

    create formatter class

---
+++ classes/Formatter.py
@@ -0,0 +1,50 @@
+class Formatter:
+    def __init__(self,text):
+        self.text = text
+    
+    def format():
+        pass
+
+class StandardMeasurementTextFormatter(Formatter):
+    def __init__(self, text):
+        super().__init__(text)
+
+    def format(self, text):
+        """ 
+        Format standard height measurement text input.
+
+        Input:
+            "feet,inches" ; ex: "5,2"
+        Output:
+            (int(feet), int(inches)); ex: (5, 2)
+        """
+
+        height = text.split(",")
+        
+        try: 
+            height_tup = (float(height[0]), float(height[1]))
+        except:
+            print("Input must contain only numbers separated by \",\". Ex: \"5,2\" for 5 feet, 2 inches")
+
+        return height_tup
+    
+class Text2FloatFormatter(Formatter):
+    def __init__(self, text):
+        super().__init__(text)
+
+    def format(self, text):
+        """ 
+        Convert any text number to a float.
+
+        Input:
+            "50.2"
+        Output:
+            float(50.2)
+        """
+
+        try: 
+            f_text = float(text)
+        except:
+            print("Input must contain only numbers")  
+        
+        return f_text
