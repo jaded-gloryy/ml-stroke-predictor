e0abda0dbfa4f1048962394bde48a52b3c951da7
Jade Burgin <jjr1945@rit.edu>
2023-08-18 09:38:12 -0400

    add files for HF app to reference

--- config.py
+++ config.py
@@ -3,8 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CONFIG = {
-    "DATA_FILEPATH": environ["DATA_FILEPATH"],
-    "CHILD_BMI": environ["CHILD_BMI"],
-    "NOT_DOC": environ["NOT_DOC"]
-}
+try:
+    CONFIG = {
+        "DATA_FILEPATH": environ["DATA_FILEPATH"],
+        "CHILD_BMI": environ["CHILD_BMI"],
+        "NOT_DOC": environ["NOT_DOC"]
+    }
+except KeyError:
+    CONFIG = {
+        "DATA_FILEPATH": "data/healthcare-dataset-stroke-data.csv",
+        "CHILD_BMI": "data/bmiagerev.csv",
+        "NOT_DOC": "data/notdoc.png"
+    }
